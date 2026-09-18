8026823f3ccb580c01ae5f90f32f73a5bec70e8d
Geoffrey Argence <argence.geoffrey@gmail.com>
2026-09-18 07:23:41 +0200

    TMP [P3]: Apply P2 manifests on the k3d server, as a proof-of-concept (to revert)

--- P3/scripts/deploy.sh
+++ P3/scripts/deploy.sh
@@ -1,3 +1,7 @@
 SCRIPTS_DIR=$(dirname $0)
 CONFS_DIR=${SCRIPTS_DIR}/../confs
-k3d cluster create --config ${CONFS_DIR}/k3d-config.yaml
+P2_CONFS_DIR=${SCRIPTS_DIR}/../../P2/confs
+k3d cluster create --config ${CONFS_DIR}/k3d-config.yaml &&
+	kubectl apply -f ${P2_CONFS_DIR}/deployments.yaml &&
+	kubectl apply -f ${P2_CONFS_DIR}/services.yaml &&
+	kubectl apply -f ${P2_CONFS_DIR}/ingress.yaml
