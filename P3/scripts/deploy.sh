a396e55adc43dde0cad91beabb3fb892b2791fe2
Geoffrey Argence <argence.geoffrey@gmail.com>
2026-09-19 06:24:05 +0200

    Revert "TMP [P3]: Apply P2 manifests on the k3d server, as a proof-of-concept (to revert)"
    This reverts commit 8026823f3ccb580c01ae5f90f32f73a5bec70e8d.

--- P3/scripts/deploy.sh
+++ P3/scripts/deploy.sh
@@ -1,7 +1,3 @@
 SCRIPTS_DIR=$(dirname $0)
 CONFS_DIR=${SCRIPTS_DIR}/../confs
-P2_CONFS_DIR=${SCRIPTS_DIR}/../../P2/confs
-k3d cluster create --config ${CONFS_DIR}/k3d-config.yaml &&
-	kubectl apply -f ${P2_CONFS_DIR}/deployments.yaml &&
-	kubectl apply -f ${P2_CONFS_DIR}/services.yaml &&
-	kubectl apply -f ${P2_CONFS_DIR}/ingress.yaml
+k3d cluster create --config ${CONFS_DIR}/k3d-config.yaml
