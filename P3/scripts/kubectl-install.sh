99b14453f125fdc82cace3e46c185fc198d6eeb4
Geoffrey Argence <argence.geoffrey@gmail.com>
2026-09-19 18:24:35 +0200

    FEAT [P3/kubectl-install.sh]: Permit to setup executable name from env

--- P3/scripts/kubectl-install.sh
+++ P3/scripts/kubectl-install.sh
@@ -1,7 +1,8 @@
 set -eux
 cd /tmp
 KUBECTL_VERSION=${KUBECTL_VERSION:-$(curl -L -s https://dl.k8s.io/release/stable.txt)}
+KUBECTL=${KUBECTL:-kubectl}
 curl -LO "https://dl.k8s.io/release/${KUBECTL_VERSION}/bin/linux/amd64/kubectl"
 expected=$(curl -L -s https://dl.k8s.io/${KUBECTL_VERSION}/bin/linux/amd64/kubectl.sha256)
 echo "${expected}  kubectl" | sha256sum -c -
-sudo install -m 0755 kubectl /usr/local/bin/
+sudo install -m 0755 kubectl "/usr/local/bin/${KUBECTL}"
