ca8152bd2abf61dfba6dc8db644a76026ecdba99
Geoffrey Argence <argence.geoffrey@gmail.com>
2026-09-19 18:07:58 +0200

    FEAT [P3]: Use set -eux and do not overwrite target version for k3d-install.sh and kubectl.sh

--- P3/scripts/k3d-install.sh
+++ P3/scripts/k3d-install.sh
@@ -1,10 +1,9 @@
-K3D_VERSION=v5.9.0
+set -eux
+K3D_VERSION=${K3D_VERSION:-v5.9.0}
 cd /tmp
 base="https://github.com/k3d-io/k3d/releases/download/${K3D_VERSION}"
 curl -sSfLO "${base}/k3d-linux-amd64"
 curl -sSfLO "${base}/checksums.txt"
-# Vérifier l'empreinte SHA256 avant d'installer
 expected=$(grep 'k3d-linux-amd64$' checksums.txt | awk '{print $1}')
-echo "${expected}  k3d-linux-amd64" | sha256sum -c - &&
-	sudo install -m 0755 k3d-linux-amd64 /usr/local/bin/k3d &&
-	k3d version
+echo "${expected}  k3d-linux-amd64" | sha256sum -c -
+sudo install -m 0755 k3d-linux-amd64 /usr/local/bin/k3d
--- P3/scripts/kubectl-install.sh
+++ P3/scripts/kubectl-install.sh
@@ -1,7 +1,7 @@
+set -eux
 cd /tmp
-STABLE_VERSION=$(curl -L -s https://dl.k8s.io/release/stable.txt)
-curl -LO "https://dl.k8s.io/release/${STABLE_VERSION}/bin/linux/amd64/kubectl"
-expected=$(curl -L -s https://dl.k8s.io/${STABLE_VERSION}/bin/linux/amd64/kubectl.sha256)
-echo "${expected}  kubectl" | sha256sum -c - &&
-	sudo install -m 0755 kubectl /usr/local/bin/ &&
-	kubectl version --client
+KUBECTL_VERSION=${KUBECTL_VERSION:-$(curl -L -s https://dl.k8s.io/release/stable.txt)}
+curl -LO "https://dl.k8s.io/release/${KUBECTL_VERSION}/bin/linux/amd64/kubectl"
+expected=$(curl -L -s https://dl.k8s.io/${KUBECTL_VERSION}/bin/linux/amd64/kubectl.sha256)
+echo "${expected}  kubectl" | sha256sum -c -
+sudo install -m 0755 kubectl /usr/local/bin/
